2ee11b4f48da36d17f8cc3881942b8ed0ec8c0bd
datphan99 <110714036+datphan99@users.noreply.github.com>
2026-01-29 08:21:46 +0700

    Update employment date for Frontend Developer role

--- src/data/about.ts
+++ src/data/about.ts
@@ -19,7 +19,7 @@ export const about: About[] = [
     id: "ADVN",
     workPlace: "ADVN Sofware",
     position: "Frontend Developer",
-    date: "Jul 2024 - Dec 2024",
+    date: "Nov 2024 - Dec 2024",
     description:
       "Supported daily operations, optimized website performance with efficient coding practices, and developed interactive features using Next.js to improve user engagement and overall user experience.",
   },
